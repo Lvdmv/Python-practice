cc6feaf15f5aa051419395500f95efafa9e6dc80
user1 <lvdmv@live.ru>
2021-07-05 22:37:44 +0300

    сделано

--- 15_IDE/03_sum_and_dif/main.py
+++ 15_IDE/03_sum_and_dif/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+
+def summNum(number):
+    summ = 0
+    while number != 0:
+        summ = number % 10
+        number //= 10
+    return summ
+
+def countNum(number):
+    count = 0
+    while number != 0:
+        number //= 10
+        count += 1
+    return count
+
+
+n = int(input('Введите число: '))
+print('Сумма цифр:', summNum(n))
+print('Кол-во цифр в числе:', countNum(n))
+print('Разность суммы и кол-ва цифр:', summNum(n) - countNum(n))
