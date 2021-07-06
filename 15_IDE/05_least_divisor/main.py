87a603d1eff7ca6c302d71c66b53cf5b4c47167e
user1 <lvdmv@live.ru>
2021-07-06 21:47:37 +0300

    сделано

--- 15_IDE/05_least_divisor/main.py
+++ 15_IDE/05_least_divisor/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+n = int(input('Введите число: '))
+count = 2
+while n % count != 0:
+    if count == 9:
+        count = n
+        break
+    else:
+        count += 1
+print('Наименьший делитель, отличный от единицы:', count)
