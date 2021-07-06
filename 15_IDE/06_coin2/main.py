d1bee7d552a58dd3be9624e108f757f4ddb10df4
user1 <lvdmv@live.ru>
2021-07-06 22:25:06 +0300

    сделано

--- 15_IDE/06_coin2/main.py
+++ 15_IDE/06_coin2/main.py
@@ -1,9 +1,11 @@
 
-print('Введите координаты монетки:')
-x = float(input('X: '))
-y = float(input('Y: '))
-r = int(input('Введите радиус: '))
-if r >= x and r >= y:
-    print('Монетка где-то рядом')
-else:
-    print('Монетки в области нет')
+def coin():
+  print('Введите координаты монетки:')
+  x = float(input('X: '))
+  y = float(input('Y: '))
+  r = int(input('Введите радиус: '))
+  if r >= x and r >= y:
+      print('Монетка где-то рядом')
+  else:
+      print('Монетки в области нет')
+coin()
