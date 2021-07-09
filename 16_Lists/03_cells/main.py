38334cda8b0463d15270e96b79b04986884eeb0f
user1 <lvdmv@live.ru>
2021-07-09 20:09:57 +0300

    сделано

--- 16_Lists/03_cells/main.py
+++ 16_Lists/03_cells/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+n = int(input('Введите кол-во клеток: '))
+cage_list =[]
+for i in range(n):
+  print('Эффективность', i + 1, 'клетки:', end ='')
+  cage_effect = int(input())
+  cage_list.append(cage_effect)
+print('\nНеподходящие значения: ', end =' ')
+for i in range(n):
+  if  cage_list[i] < i + 1:
+    print(cage_list[i], end=' ')
