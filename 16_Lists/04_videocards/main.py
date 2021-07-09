bf5bca801a55793346c1dfa487036d85eda567c0
user1 <lvdmv@live.ru>
2021-07-09 20:29:57 +0300

    сделано

--- 16_Lists/04_videocards/main.py
+++ 16_Lists/04_videocards/main.py
@@ -1,3 +1,24 @@
-# TODO здесь писать код
+n = int(input('Кол-во видеокарт: '))
+videocard_list = []
+
+for i in range(n):
+  print(i + 1, 'Видеокарта: ', end ='')
+  v_card = int(input())
+  videocard_list.append(v_card)
+
+maxCard = videocard_list[0]
+
+for cardMaxnum in videocard_list:
+  if cardMaxnum > maxCard:
+    maxCard = cardMaxnum
+
+newcard_list = []
+
+for cardNum in videocard_list:
+  if cardNum != maxCard:
+    newcard_list.append(cardNum)
+
+print('Старый список видеокарт:', videocard_list)
+print('Новый список видеокарт:', newcard_list)
 
 
