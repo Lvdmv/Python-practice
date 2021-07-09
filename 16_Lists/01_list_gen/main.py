9dc6679dff72cbe1cd31de8a4659289ac193c311
user1 <lvdmv@live.ru>
2021-07-09 20:09:57 +0300

    сделано

--- 16_Lists/01_list_gen/main.py
+++ 16_Lists/01_list_gen/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+n = int(input('Сколько всего чисел? '))
+countOddnum =[]
+for i in range(1, n + 1):
+  if i % 2 != 0:
+    countOddnum.append(i)
+print('В списке чисел от 1 до', n, 'содержутся следующие нечетные числа:', countOddnum)
