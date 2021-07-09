da22b4c5cb7f90e2ef4f354cf45fa1d79a4224be
user1 <lvdmv@live.ru>
2021-07-09 22:57:26 +0300

    сделано

--- 16_Lists/06_word_analysis/main.py
+++ 16_Lists/06_word_analysis/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+word = input('Введите слово: ')
+unique_list = list(word)
+counter = len(unique_list)
+countNum = [0, 0]
+for num in range(counter):
+    flag = True
+    for i in unique_list:
+        if unique_list[num] == i and countNum[0] != num:
+            flag = False
+        countNum[0] += 1
+    countNum[0] = 0
+    if flag:
+        countNum[1] += 1
+print('\nКол-во уникальных букв: ', countNum[1])
