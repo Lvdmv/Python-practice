5bf403a0b39731d8eecae0f7dfd0b50577406cde
user1 <lvdmv@live.ru>
2021-07-09 22:57:26 +0300

    сделано

--- 16_Lists/02_tournament/main.py
+++ 16_Lists/02_tournament/main.py
@@ -2,5 +2,5 @@ team_list =['Артемий', 'Борис', 'Влад', 'Гоша', 'Дима', 
 evenCount = []
 print('Состав команды: ', end ='\n')
 for i in range(8):
-  if i % 2 != 0:
+  if i % 2 == 0:
     print(team_list[i])
--- 16_Lists/07_containers/main.py
+++ 16_Lists/07_containers/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+containers = int(input('Кол-во контейнеров: '))
+num_conteiners = []
+for _ in range(containers):
+    container = int(input('Введите вес контейнера: '))
+    num_conteiners.append(container)
+x = int(input('Введите вес нового контейнера: '))
+num_conteiners = num_conteiners.append(x)
+coun = 0
+for num in num_conteiners:
+    coun += 1
+index = 0
+for i in range(coun):
+    if x > num_conteiners[i]:
+        index = i + 1
+        break
+print('Номер, куда встанет новый контейнер:', index)
