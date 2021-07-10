14437503ac8204b4896d29285fad32b935698f5a
user1 <lvdmv@live.ru>
2021-07-10 12:16:13 +0300

    сделано

--- 16_Lists/09_word_analysis_2/main.py
+++ 16_Lists/09_word_analysis_2/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+word = input('Введите слово: ')
+word_list = list(word)
+palindrome = []
+for i in range(1, len(word_list) + 1):
+    palindrome.append(word_list[-i])
+if palindrome == word_list:
+    print('Слово является палиндромом')
