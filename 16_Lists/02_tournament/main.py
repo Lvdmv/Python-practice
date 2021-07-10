8d06f2d2ad7ae174bf59944ee2b0743685786ad2
user1 <lvdmv@live.ru>
2021-07-10 23:24:38 +0300

    Задача 2. Турнир -  сделана

--- 16_Lists/02_tournament/main.py
+++ 16_Lists/02_tournament/main.py
@@ -1,6 +1,5 @@
 team_list =['Артемий', 'Борис', 'Влад', 'Гоша', 'Дима', 'Евгений', 'Женя', 'Захар']
 evenCount = []
 print('Состав команды: ', end ='\n')
-for i in range(8):
-  if i % 2 == 0:
+for i in range(0, 8, 2):
     print(team_list[i])
--- 16_Lists/10_sort/main.py
+++ 16_Lists/10_sort/main.py
@@ -1,19 +1,35 @@
-n = int(input('Введите кол-во чисел: '))
-digit_list = []
-for i in range(n):
-    digit = int(input('Введите число: '))
-    digit_list.append(digit)
-print('Изначальный список:', digit_list)
-index = 0
-count = 0
-for _ in range(n):
-    minNum = digit_list[count]
-    for j in range(n - count):
-        if minNum > digit_list[j + count]:
-            minNum = digit_list[count + j]
-            index = count + j
-    digit_list[index] = digit_list[count]
-    digit_list[count] = minNum
-    count += 1
-    index = count
-print('Отсортированный список:', digit_list)
+# n = int(input('Введите кол-во чисел: '))
+# digit_list = []
+# for i in range(n):
+#     digit = int(input('Введите число: '))
+#     digit_list.append(digit)
+# print('Изначальный список:', digit_list)
+# index = 0
+# count = 0
+# for _ in range(n):
+#     minNum = digit_list[count]
+#     for j in range(n - count):
+#         if minNum > digit_list[j + count]:
+#             minNum = digit_list[count + j]
+#             index = count + j
+#     digit_list[index] = digit_list[count]
+#     digit_list[count] = minNum
+#     count += 1
+#     index = count
+# print('Отсортированный список:', digit_list)
+
+
+# def selection_sort(my_list):
+#     for i_am in range(len(my_list)):
+#         for curr in range(i_am, len(my_list)):
+#             if my_list[curr] < my_list[i_am]:
+#                 my_list[i_am], my_list[curr] = my_list[curr], my_list[i_am]
+#
+#
+#
+#
+# nums = [1, 4, -3, 0, 10]
+#
+# selection_sort(nums)
+#
+# print(nums)
