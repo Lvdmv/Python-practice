296a6b15ea44fc01284222c7628ee55163022c2e
user1 <lvdmv@live.ru>
2021-07-10 12:03:28 +0300

    сделано

--- 16_Lists/08_running_nums/main.py
+++ 16_Lists/08_running_nums/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+k = int(input('Сдвиг: '))
+n = int(input('Введите кол-во элементов в списке: '))
+normal_list = []
+shifted_list = []
+for i in range(n):
+    element = int(input('Введите элемент списка: '))
+    normal_list.append(element)
+for i in range(n):
+    shifted_list.append(normal_list[i - k])
+print('Изначальный список: ', normal_list)
+print('Сдвинутый список: ', shifted_list)
