cb088c680862ca412933691832ce2eb3e5618513
user1 <lvdmv@live.ru>
2021-07-18 23:56:23 +0300

    Задача 3. Детали- сделана

--- 17_List_methods/03_details/main.py
+++ 17_List_methods/03_details/main.py
@@ -1,1 +1,19 @@
+shop = [['каретка', 1200], ['шатун', 1000], ['седло', 300],
+        ['педаль', 100], ['седло', 1500], ['рама', 12000],
+        ['обод', 2000], ['шатун', 200], ['седло', 2700]]
+
+detail = input('Название детали: ')
+counter = [0, 0]
+
+for i_prace in range(len(shop)):
+        flag = True
+        if shop[i_prace][0] == detail:
+                flag = False
+                counter[0] += shop[i_prace][1]
+                counter[1] += 1
+if flag:
+        print('Такой детали нет в наличии.')
+else:
+        print('Кол-во деталей: ', counter[1])
+        print('Общая стоимость: ', counter[0])
 
