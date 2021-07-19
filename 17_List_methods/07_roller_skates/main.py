ed67248fd5ae0616fdab090ce3b79b74db10ff5f
user1 <lvdmv@live.ru>
2021-07-19 16:07:19 +0300

    Задача 7. Ролики- сделана

--- 17_List_methods/07_roller_skates/main.py
+++ 17_List_methods/07_roller_skates/main.py
@@ -1,18 +1,3 @@
-def countSkaters(my_list_1, my_list_2, i, j):
-    skaters = 0
-    for i_sk in range(1, my_list_1.count(my_list_1[j]) + 1):
-        if i_sk == my_list_1.count(my_list_1[i]):
-            if i_sk > my_list_2.count(my_list_2[i]):
-                skaters += my_list_2.count(my_list_2[i])
-            else:
-                skaters += my_list_1.count(my_list_1[j])
-    return skaters
-
-def delIndex(my_list, j):
-    size = my_list[j]
-    for _ in range(my_list.count(size)):
-        my_list.remove(size)
-
 
 skates_amt = int(input('Кол-во коньков: '))
 skates_list = []
@@ -28,40 +13,22 @@ for i_skates_amt in range(skates_men):
     print('Размер ноги ', i_skates_amt + 1, ' человека: ', end = ' ')
     skater = int(input())
     skaters_list.append(skater)
-
 count = 0
-i = 0
-while len(skaters_list) != 0:
-    flag = True
-    for num in range(len(skates_list)):
-        if skaters_list[i] == skates_list[num]:
-            flag = False
-            if skaters_list.count(skaters_list[i]) == skates_list.count(skates_list[num]):
-                count += skaters_list.count(skaters_list[i])
-                delIndex(skates_list, num)
-                delIndex(skaters_list, i)
-                break
-            else:
-                count += countSkaters(skates_list, skaters_list, i, num)
-                delIndex(skates_list, num)
-                delIndex(skaters_list, i)
-                break
-    if flag:
-        delIndex(skaters_list, i)
+for i in sorted(skaters_list):
+    for num in sorted(skates_list):
+        if i == num:
+          if skaters_list.count(i) <= skates_list.count(num):
+              count += skaters_list.count(i)
+              index = i
+          else:
+              count += skates_list.count(num)
+              index = num
+          for _ in range(skaters_list.count(index)):
+              skaters_list.remove(index)
+          index = 0
+          break
+
 print('\nНаибольшее кол-во людей, которые могут взять ролики: ', count)
 
 
 
-
-# #Кол-во коньков: 4
-# Размер 1 пары: 41
-# Размер 2 пары: 40
-# Размер 3 пары: 39
-# Размер 4 пары: 42
-#
-# Кол-во людей: 3
-# Размер ноги 1 человека: 42
-# Размер ноги 2 человека: 41
-# Размер ноги 3 человека: 42
-#
-# Наибольшее кол-во людей, которые могут взять ролики: 2
