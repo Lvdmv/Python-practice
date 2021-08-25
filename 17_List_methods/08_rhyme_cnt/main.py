b1956605ccc6dcab70439e82ac936b4419eadc7b
user1 <lvdmv@live.ru>
2021-08-25 23:25:38 +0300

    Задача 8. Считалка

--- 17_List_methods/08_rhyme_cnt/main.py
+++ 17_List_methods/08_rhyme_cnt/main.py
@@ -1,34 +1,22 @@
-def delIndex(startIndex, k, my_list):
-    if k >= len(my_list):
-        nextIndex = startIndex + k - (k // len(my_list)) * len(my_list) - 1
-        while nextIndex > len(my_list) - 1:
-            nextIndex %= len(my_list)
-    else:
-        nextIndex = startIndex + k - 1
-        while nextIndex > len(my_list) - 1:
-            nextIndex %= len(my_list)
-    return nextIndex
-
-def startPoint(k, my_list):
-    n = delIndex(startIndex, k, n_list)
-    if n != len(my_list) - 1 and n > 0:
-      return n
-    else:
-      return 0
+def delIndex(my_list, index):
+  nextIndex = index % len(my_list)
+  return nextIndex
 
 n = int(input('Кол-во человек: '))
 k = int(input('Какое число в считалке? '))
 print('Значит, выбывает каждый ', k, 'человек')
 n_list = list(range(1, n + 1))
-startIndex= 0
+startIndex = 0
 start = 0
+dell = 0
 while len(n_list) != 1:
   print('Текущий круг:', n_list)
-  print('Начало счёта с номера', n_list[startIndex])
-  dell = n_list[delIndex(startIndex, k, n_list)]
-  print('Выбывает человек под номером', dell)
-  start = startPoint(k, n_list)
-  n_list.remove(dell)
-  startIndex = start
-
-print('\nОстался человек под номером', n_list[0])
+  print('Начало счёта с номера: ', n_list[startIndex])
+  print('Будет удалена: ', n_list[delIndex(n_list, k + startIndex-1)])
+  start = delIndex(n_list, k + startIndex-1)
+  n_list.remove(n_list[delIndex(n_list, k + startIndex-1)])
+  if start <= len(n_list)-1:
+    startIndex = start
+  else:
+     startIndex = 0
+print('Останется число: ', n_list[0])
