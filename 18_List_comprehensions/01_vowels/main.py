599a460c68264474f71490c3a0f91f63736eed7a
Виктор Трикозов <lvdmv@live.ru>
2021-08-28 14:34:43 +0000

    Update 18_List_comprehensions/01_vowels/main.py

--- 18_List_comprehensions/01_vowels/main.py
+++ 18_List_comprehensions/01_vowels/main.py
@@ -1,5 +1,5 @@
 vowels = 'аиеёоуыэюя'
 text = input('Введите текст: ')
-list = [text[i] for i in range(len(text)) for j in range(len(vowels)) if text[i] == vowels[j]]
-print('Список гласных букв: ',list)
-print('Длина списка: ', len(list))
+my_list = [i for i in text if i in vowels]
+print('Список гласных букв: ',my_list)
+print('Длина списка: ', len(my_list))
