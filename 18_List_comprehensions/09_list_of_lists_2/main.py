dd8ea9a0a1208296380c232348479bfd093d01b4
Виктор Трикозов <lvdmv@live.ru>
2021-08-28 17:30:18 +0000

    Update 18_List_comprehensions/09_list_of_lists_2/main.py

--- 18_List_comprehensions/09_list_of_lists_2/main.py
+++ 18_List_comprehensions/09_list_of_lists_2/main.py
@@ -1,6 +1,6 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-new_list = []
-nice_list = [[new_list.extend(x) for x in nice_list[0]], [new_list.extend(x) for x in nice_list[1]]]
+new_list = [x for y in nice_list for z in y for x in z]
+
 print(new_list)
