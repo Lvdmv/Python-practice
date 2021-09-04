411116d05e3af11e81234e7d014cddb2048f084f
user1 <lvdmv@live.ru>
2021-09-04 15:36:23 +0300

    Задача 7. IP - адрес 2 - сделана

--- 19_Strings/07_ip_adress_2/main.py
+++ 19_Strings/07_ip_adress_2/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+str_ip = input('Введите IP: ')
+if len(str_ip.split('.')) != 4:
+    print('Адрес - это четыре числа, разделённые точками')
+
+elif [i_elem for i_elem in str_ip.split('.') if not i_elem.isdigit()]:
+    print(' '.join([i_elem for i_elem in str_ip.split('.') if not i_elem.isdigit()]), '- не целое число')
+
+elif [i_elem for i_elem in str_ip.split('.') if int(i_elem) > 255]:
+    print(' '.join([i_elem for i_elem in str_ip.split('.') if int(i_elem) > 255]), 'превышает 255')
+else:
+    print('IP-адрес корректен')
+
+print(str_ip)
