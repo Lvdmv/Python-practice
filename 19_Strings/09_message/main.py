b36fef1443229b2008b5ceab0a3150ffd323bd91
user1 <lvdmv@live.ru>
2021-09-06 18:31:09 +0300

    Задача 9. Сообщение - сделана

--- 19_Strings/09_message/main.py
+++ 19_Strings/09_message/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+def reverse_str(i_word):
+    rev_word = ''
+    revers = ' '
+    flag = True
+    for i in i_word:
+        if i.isalpha() and flag:
+            rev_word = i + rev_word
+        elif not flag:
+            revers = i + revers
+        else:
+            rev_word = rev_word + i
+            flag = False
+    return rev_word + revers
+
+text = input('Сообщение: ')
+print(''.join([reverse_str(i_elem) for i_elem in (''.join([i_elem for i_elem in list(text)]).split(' '))]))
+
