41ebe297d62b64eacade80616adc40571761f24b
user1 <lvdmv@live.ru>
2021-09-20 11:25:51 +0300

    Задача 5. Гистограмма частоты 2 - сделана

--- 20_Dictionaries_and_sets/05_frequency_hist_2/main.py
+++ 20_Dictionaries_and_sets/05_frequency_hist_2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+text = input('Введите текст: ')
+print('Оригинальный словарь частот:', end = '\n')
+sym_str = dict()
+for i_let in text:
+    if i_let in sym_str:
+        sym_str[i_let] += 1
+    else:
+        sym_str[i_let] = 1
+for i_num in sorted(sym_str.keys()):
+    print(i_num, ':', sym_str[i_num])
+print('\nИнвертированный словарь частот:\n')
+for i_word in set(sym_str.values()):
+    print(i_word, ':', [i_key for i_key in sym_str.keys() if sym_str[i_key] == i_word])
+    print()
