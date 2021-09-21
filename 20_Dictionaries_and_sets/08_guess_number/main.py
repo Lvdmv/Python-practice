f4e019a7d9690eed35f262f725ed5a950aa723ab
user1 <lvdmv@live.ru>
2021-09-21 23:19:07 +0300

    Задача 8. Угадай число - сделана

--- 20_Dictionaries_and_sets/08_guess_number/main.py
+++ 20_Dictionaries_and_sets/08_guess_number/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+import random
+
+max_num = int(input('Введите максимальное число: '))
+num_set = {i for i in range(1, max_num + 1)}
+conceived_num = random.randint(1, max_num)
+print(conceived_num)
+
+while True:
+    print('Нужное число есть среди вот этих чисел: ', end = '')
+    answer = input().lower()
+    if answer == 'помогите!':
+        print('Артём мог загадать следующие числа: ', end = '')
+        for i_set in num_set:
+            print(i_set, end = ' ')
+        break
+    elif answer == str(conceived_num):
+        print('Ты угадал!')
+        break
+    elif str(conceived_num) in answer.split():
+        print('Ответ Артёма: Да')
+        for i_num in (num_set - set({int(str_num) for str_num in answer.split()})):
+            num_set.discard(i_num)
+    else:
+        print('Ответ Артёма: Нет')
+        for i_num in set({int(str_num) for str_num in answer.split()}):
+            num_set.discard(i_num)
+
+
