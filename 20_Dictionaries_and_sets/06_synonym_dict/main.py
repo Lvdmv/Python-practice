a0c72864dd8887c45113a023daa75f0ab30ca699
Виктор Трикозов <lvdmv@live.ru>
2021-09-30 05:50:28 +0000

    Update 20_Dictionaries_and_sets/06_synonym_dict/main.py

--- 20_Dictionaries_and_sets/06_synonym_dict/main.py
+++ 20_Dictionaries_and_sets/06_synonym_dict/main.py
@@ -1,17 +1,18 @@
 couple_words = int(input('Введите кол-во пар слов: '))
 
+couple_dict = dict()
 for i_word in range(couple_words):
-    print(i_word + 1, 'пара:', end = '')
-    couple_word =
+    print(i_word + 1, 'пара: ', end='')
+    couple_word = input().split()
+    couple_dict[couple_word[2]] = couple_word[0]
+    couple_dict[couple_word[0]] = couple_word[2]
 
+print()
+while True:
+    word = input('Введите слово: ').title()
+    if couple_dict.get(word):
+        print('Синоним:', couple_dict[word])
+        break
+    else:
+        print('Такого слова в словаре нет.')
 
-#
-# Введите количество пар слов: 3
-# 1 пара: Привет - Здравствуйте
-# 2 пара: Печально - Грустно
-# 3 пара: Весело - Радостно
-#
-# Введите слово: интересно
-# Такого слова в словаре нет.
-# Введите слово: здравствуйте
-# Синоним: Привет
