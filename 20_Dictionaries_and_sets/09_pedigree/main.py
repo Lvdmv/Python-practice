6102eda62c5cc4c13b937d5126806b2a42818d4a
Виктор Трикозов <lvdmv@live.ru>
2021-10-05 19:12:25 +0000

    Update 20_Dictionaries_and_sets/09_pedigree/main.py

--- 20_Dictionaries_and_sets/09_pedigree/main.py
+++ 20_Dictionaries_and_sets/09_pedigree/main.py
@@ -3,23 +3,19 @@ def height(my_key, my_dict):
         for i_key in i.keys():
             if my_key == i_key:
                 return i[i_key] + 1
-            
-def split_str(string, index):
-    return string.split()[index]
 
 amt_person = int(input('Введите кол-во человек: '))
 pair_dict = {'Peter_I': {'Peter_I': 0}}
 for i_person in range(amt_person - 1):
     print(i_person + 1, 'пара: ', end='')
-    pair = input()
-    if not pair.split()[1] in pair_dict.keys():
-        pair_dict[split_str(pair, 1)] = {split_str(pair, 0): height(split_str(pair, 1), pair_dict.values())}
-
+    pair = input().split()
+    if not pair[1] in pair_dict.keys():
+        pair_dict[pair[1]] = {pair[0]: height(pair[1], pair_dict.values())}
     else:
-        pair_dict[split_str(pair, 1)][split_str(pair, 0)] = height(split_str(pair, 1), pair_dict.values())
-print(pair_dict)
+        pair_dict[pair[1]][pair[0]] = height(pair[1], pair_dict.values())
 print()
 print('"Высота" каждого члена семьи: ')
 for i_pair in sorted({i: j[i] for j in pair_dict.values() for i in j}.keys()):
     print(i_pair, {i: j[i] for j in pair_dict.values() for i in j}[i_pair])
+
 
