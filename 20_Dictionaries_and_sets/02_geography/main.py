0a947624b06e50dd3d4ea9512b436ec21cdaddd8
Виктор Трикозов <lvdmv@live.ru>
2021-10-05 19:16:38 +0000

    Update 20_Dictionaries_and_sets/02_geography/main.py

--- 20_Dictionaries_and_sets/02_geography/main.py
+++ 20_Dictionaries_and_sets/02_geography/main.py
@@ -1,26 +1,21 @@
-def city_location(city):
-    for i_city in dict_country.values():
-        if city in i_city:
-            return i_city
-    else:
-        return False
-def split_str(string):
-    return string.split()
+def city_location(my_dict, my_city):
+    return ''.join({i_key for i_key in my_dict.keys() if my_city in my_dict[i_key]})
 
 amt_countries = int(input('Кол-во стран: '))
 
 dict_country = dict()
 
 for i_country in range(amt_countries):
-    print(i_country + 1, 'страна: ', end = '')
-    country_and_city = input()
-    dict_country[split_str(country_and_city)[0]] = split_str(country_and_city)[1:]
+    print(i_country + 1, 'страна: ', end='')
+    country_and_city = input().split()
+    dict_country[country_and_city[0]] = country_and_city[1:]
 
 for i_num in range(3):
     print()
-    print(i_num + 1, 'город: ', end= '')
+    print(i_num + 1, 'город: ', end='')
     city = input()
-    if city_location(city):
-        print('Город', city, 'расположен в стране', ''.join({i_key for i_key in dict_country.keys() if dict_country[i_key] == city_location(city)}) + '.')
+    if city_location(dict_country, city):
+        print(f'Город {city} расположен в стране {city_location(dict_country, city)}.')
     else:
-        print('По городу', city, 'данных нет.')
+        print(f'По городу {city} данных нет.')
+
