9b6f9ba6dda7407be2d43220d7e7d6eeb4655fe6
user1 <lvdmv@live.ru>
2021-10-19 15:58:30 +0300

    Задача 7. Функция сортировки - решена

--- 21_Tuples/07_sort_function/main.py
+++ 21_Tuples/07_sort_function/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def sorted_tuple(my_tuple):
+    if {i for i in my_tuple if isinstance(i, float)}:
+        return tuple(my_tuple)
+    else:
+        return tuple(sorted(my_tuple))
+
+
+# origin_tuple = (1, 4, 3, 2)
+origin_tuple = (1, 2, 3.0, 2)
+# print(origin_tuple)
+print(sorted_tuple(origin_tuple))
