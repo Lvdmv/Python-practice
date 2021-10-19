6fff362f2cb7be1783fc69fd62226694d9b70243
user1 <lvdmv@live.ru>
2021-10-19 13:12:53 +0300

    Задача 6. По парам - решена

--- 21_Tuples/06_pairs/main.py
+++ 21_Tuples/06_pairs/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+import random
+
+# Первый вариант:
+def pair_tuple(my_list):
+   tuple_even = []
+   tuple_odd = []
+   for i, j in enumerate(my_list):
+       if i % 2 == 0:
+         tuple_even.append(j)
+       else:
+          tuple_odd.append(j)
+   new_tuple = zip(tuple_even, tuple_odd)
+   new_tuple = list(new_tuple)
+   return new_tuple
+
+# Второй вариант:
+def pair_tuple(my_list):
+    return list(zip(my_list[::2], my_list[1::2]))
+
+
+origin_list = sorted([random.randint(0, 10) for _ in range(10)])
+
+print('Оригинальный список', origin_list)
+print('Новый список', pair_tuple(origin_list))
+
+
