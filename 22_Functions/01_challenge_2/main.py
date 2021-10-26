d351efa4ba218ab732176404c3517a8ffbc83362
user1 <lvdmv@live.ru>
2021-10-26 08:24:35 +0300

    Задача 1. Challenge 2 - сделана

--- 22_Functions/01_challenge_2/main.py
+++ 22_Functions/01_challenge_2/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def sequance_num(num):
+    if num == 0:
+        return 1
+    sequance_num(num - 1)
+    print(num, end=' ')
+
+
+number = int(input('Введите число: '))
+sequance_num(number)
+
