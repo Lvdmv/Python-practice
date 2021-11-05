f4fd86a1dcc10e9bc43d1a512c7eb657965a39ab
user1 <lvdmv@live.ru>
2021-11-05 19:57:34 +0300

    Задача 6. Глубокое копирование - сделана

--- 22_Functions/06_deep_copy/main.py
+++ 22_Functions/06_deep_copy/main.py
@@ -11,4 +11,27 @@ site = {
     }
 }
 
-# TODO здесь писать код
+
+def function(name):
+    site['html']['head']['title'] = f'Куплю/продам {name} недорого'
+    site['html']['body']['h2'] = f'У нас самая низкая цена на {name}'
+    if name not in dict_site:
+        dict_site[name] = str(site)
+        for i in dict_site.keys():
+            print(f'\nСайт для {i}:')
+            print('site =', dict_site[i])
+            print()
+
+
+def key_position(counter):
+    if counter == 0:
+        return None
+    name_product = input('Введите название продукта для нового сайта: ')
+    function(name_product)
+    key_position(counter - 1)
+    function(name_product)
+
+
+dict_site = dict()
+amt_site = int(input('Сколько сайтов: '))
+key_position(amt_site)
