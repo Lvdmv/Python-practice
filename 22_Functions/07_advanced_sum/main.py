01f80cce10aade69a627ebdb372c2e5433ba0c12
user1 <lvdmv@live.ru>
2021-11-06 12:57:41 +0300

    Задача 7. Продвинутая функция sum - сделана

--- 22_Functions/07_advanced_sum/main.py
+++ 22_Functions/07_advanced_sum/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+def sum(*args, my_list=[]):
+    summ = 0
+    for i_num in args:
+        if str(i_num).isdigit():
+            my_list.append(i_num)
+        else:
+            for j_digit in i_num:
+                sum(j_digit)
+    for i_sum in my_list:
+        summ += i_sum
+    return summ
+
+
+print('Ответ:', sum([[1, 2, [3]], [1], 3]))
+
+print('Ответ:', sum(1, 2, 3, 4, 5, my_list=[]))
