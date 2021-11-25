ec20e939d4a7eefd0bc2e2b86411c6f78b874561
user1 <lvdmv@live.ru>
2021-11-25 12:40:26 +0300

    Задача 1. Сумма чисел 2 - сделана

--- 23_Files/01_nums_sum_2/main.py
+++ 23_Files/01_nums_sum_2/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+numbers_file = open('numbers.txt', 'r')
+data = numbers_file.read()
+print('Содержимое файла numbers.txt')
+print(data)
+summ = 0
+for i_num in data:
+    if i_num.isdigit():
+        summ += int(i_num)
+print()
+numbers_file.close()
+counts_file = open('answer.txt', 'w')
+counts_file.write(str(summ))
+print('Содержимое файла answer.txt')
+print(summ)
+counts_file.close()
