564c6b673139e39e5bbc8bbb8ac74a2f1cd95843
user1 <lvdmv@live.ru>
2021-12-04 21:00:15 +0300

    Задача 5. Текстовый калькулятор - сделана

--- 24_Exceptions/05_text_calc/main.py
+++ 24_Exceptions/05_text_calc/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+def calc_function(example):
+    try:
+        if example[1] == '+':
+            list_sum.append((int(example[0]) + int(example[2])))
+        elif example[1] == '-':
+            list_sum.append((int(example[0]) - int(example[2])))
+        elif example[1] == '*':
+            list_sum.append((int(example[0]) * int(example[2])))
+        elif example[1] == '/':
+            list_sum.append((int(example[0]) / int(example[2])))
+        elif example[1] == '%':
+            list_sum.append((int(example[0]) % int(example[2])))
+        else:
+            raise IndexError
+    except IndexError:
+        print('Ошибка в написании примера')
+    except TypeError:
+        print('Операнды должны быть числами')
+    except ZeroDivisionError:
+        print('На ноль делить нельзя')
+
+
+list_sum = []
+with open('calc.txt', 'r') as calc_file:
+    for i_operation in calc_file:
+        calc_function(i_operation.split())
+print(f'Сумма: {sum(list_sum)}')
