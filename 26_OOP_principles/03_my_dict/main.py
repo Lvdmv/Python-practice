bdfdb666b622cd8d490222d935229e4c991724c4
user1 <lvdmv@live.ru>
2022-01-17 15:12:32 +0300

    Задача 3. Свой словарь - сделано

--- 26_OOP_principles/03_my_dict/main.py
+++ 26_OOP_principles/03_my_dict/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+class MyDict:
+    def __init__(self):
+        self.my_dict = dict()
+
+    def info(self):
+        print(self.my_dict)
+
+    def save_in_dict(self, key, value):
+        self.my_dict[key] = value
+
+    def get(self, key):
+        if key in self.my_dict:
+            print(self.my_dict.get(key))
+        else:
+            print(0)
+
+
+my_dict = MyDict()
+my_dict.save_in_dict(1, 2)
+my_dict.get(1)
