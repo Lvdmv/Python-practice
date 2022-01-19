419bf6b444d23a276586281758212f3946fa243c
user1 <lvdmv@live.ru>
2022-01-19 15:53:04 +0300

    Задача 3. Свой словарь - сделана

--- 26_OOP_principles/03_my_dict/main.py
+++ 26_OOP_principles/03_my_dict/main.py
@@ -9,12 +9,21 @@ class MyDict:
         self.my_dict[key] = value
 
     def get(self, key):
-        if key in self.my_dict:
-            print(self.my_dict.get(key))
-        else:
-            print(0)
-
+        return self.my_dict.get(key, 0)
 
 my_dict = MyDict()
 my_dict.save_in_dict(1, 2)
-my_dict.get(1)
+print(my_dict.get(1))
+
+
+#
+# class MyDict(dict):
+#     def get(self, key, default=0):
+#         return super().get(key, default)
+#
+#
+# my_dict = MyDict()
+# my_dict[1] = 2
+# print(my_dict.get(1))
+
+
